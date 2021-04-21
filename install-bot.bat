4fa9848aaae31480d36c4f85c12c87b681479ac7
RealGamerDoge <75544158+RealGamerDoge@users.noreply.github.com>
2021-04-21 15:04:30 -0400

    Update and rename install-bot.bat to install-dep

--- install-bot.bat
+++
@@ -1,7 +0,0 @@
-echo "# InstaFame" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/RealGamerDoge/InstaFame.git
-git push -u origin main
